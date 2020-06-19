e1bbface9ba624563293bab86151b9654d751dca
bhamann-collab <brockohamann@gmail.com>
2020-06-19 10:42:57 +0930

    basic overlay of the front page, need to add the privacy tick option, description and an icon that acts like a logo

--- src/component/PageTitle.js
+++ src/component/PageTitle.js
@@ -1,13 +1,41 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import * as Icons from "@fortawesome/free-solid-svg-icons";
+
+//Needed to import font awesome
+const iconList = Object
+
+  .keys(Icons)
+  .filter(key => key !== "fas" && key !== "prefix" )
+  .map(icon => Icons[icon])
+
+  library.add(...iconList)
+//Needed to import font awesome
 
 const PageTitle = () => {
     return (
         <div className="titlePage">
-            <h1>This is a temporary page</h1>
-            <Link to="/pageSwipe">
-                <button>Press to Swipe</button>
-            </Link>
+            <h1>Food Tinder</h1>
+            <div className="titleButtons">
+                <Link to="/pageSwipe">
+                    <FontAwesomeIcon
+                    className="titleFontAwesome" 
+                    icon={"user"} 
+                    style={{color: "#c0392b"}}
+                    />
+                    <p>SOLO SWIPE</p>
+                </Link>
+                <Link to="/toBeReplaced">
+                    <FontAwesomeIcon 
+                    className="titleFontAwesome" 
+                    icon={"users"} 
+                    style={{color: "#c0392b"}}
+                    />
+                    <p>MULTI SWIPE</p>
+                </Link>
+            </div>
         </div>
     )
 }
