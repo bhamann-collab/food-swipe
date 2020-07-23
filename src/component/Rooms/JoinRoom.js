f341a252c3e0ef227edbb7b19ff54316f4512977
bhamann-collab <brockohamann@gmail.com>
2020-07-23 21:57:02 +0930

    Join Rooms CSS is done for now

--- src/component/Rooms/JoinRoom.js
+++ src/component/Rooms/JoinRoom.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import ToggleOnOff from './ToggleOnOff'
 
 const JoinRoom = () => {
+
+    const handleSubmit = e => {
+        e.preventDefault();
+    }
+    
     return (
-        <div>
-            JoinRoom
+        <div className="JoinRoom">
+            <div className="toggling">
+                <p>Join Room</p>
+                <ToggleOnOff />
+            </div>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <p>Enter Room Code:</p> 
+                    <input type="text"/>
+                </label>
+                <p
+                style={{color: "#27ae60"}}
+                >Room is Valid! (ExampleName)</p>
+                <button>Join Room</button>
+            </form> 
         </div>
     )
 }
