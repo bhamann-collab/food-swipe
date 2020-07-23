eb50317c131434c4b9fa7eb8c1582a83a8dadf15
bhamann-collab <brockohamann@gmail.com>
2020-07-23 22:19:20 +0930

    Added useReducer and useContext functionality to the Rooms page

--- src/component/Rooms/Rooms.js
+++ src/component/Rooms/Rooms.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import Nickname from './Nickname'
 import HostRoom from './HostRoom'
 import JoinRoom from './JoinRoom'
 import './Rooms.scss'
 
+//Use reducer to deal with the state
+export const HostContext = React.createContext()
+
+const initialStateHost = false;
+const reducer = (state, action) => {
+    switch(action) {
+        case 'setHost':
+            return true
+        case 'setJoin':
+            return false
+        default:
+            return state
+    }
+}
+
 const Rooms = () => {
+    const [isHost, dispatch] = useReducer(reducer, initialStateHost)
+
     return (
         <div className="Rooms">
-            <Nickname />
-            <HostRoom />
-            <JoinRoom /> 
+            <HostContext.Provider
+                value={{ isHostState: isHost, isHostDispatch: dispatch }}
+            >
+                <Nickname />
+                <HostRoom />
+                <JoinRoom /> 
+            </HostContext.Provider>
         </div>
     )
 }
